323b14b592f0d661b595910adcdf5b8bb1b52fdf
e.sokolova <e.sokolova@raketa.travel>
2021-04-01 16:57:26 +0300

    feat: add spme styles

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ export default function App() {
 
       </div>
       <div className="main">
+        <div className="main__image">
+          <img src="../images/1.jpg"></img>
+        </div>
         <div className="main__rollTable">
           <RollTable />
         </div>
       </div>
       <div className="footer">
-
+        ПАО «Северсталь» 2021 
       </div>
     </div>
   );
