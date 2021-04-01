0c60681d1f15a23b1285bd802d30eb855ffae78f
e.sokolova <e.sokolova@raketa.travel>
2021-04-01 17:43:02 +0300

    feat: add some styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import { RollTable } from './components/RollTable';
+import { RollPreview } from './components/RollPreview';
 import './App.css';
 
 export default function App() {
   return (
     <div className="page">
-      <div className="header">
-
-      </div>
-      <div className="main">
+      <main className="main">
         <div className="main__image">
-          <img src="../images/1.jpg"></img>
+          <RollPreview />
         </div>
         <div className="main__rollTable">
           <RollTable />
         </div>
-      </div>
-      <div className="footer">
-        ПАО «Северсталь» 2021 
-      </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/RollPreview/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './index.css';
+
+export function RollPreview() {
+    return <img className="rollPreview" src="https://www.dogbreedinfo.com/images30/PembrokeWelshCorgiPurebredDogBaozi5YearsOld.jpg" />
+};
